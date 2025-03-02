268336d645927a7d32b014dd59562a3ee53430b0
Aryan Kamat <aryankamat03@gmail.com>
2025-03-02 09:00:13 +0530

    Added circle rating bar to carousels

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -10,6 +10,7 @@ import dayjs from "dayjs";
 import ContentWrapper from "../contentWrapper/ContentWrapper";
 import Img from "../lazyLoadImage/Img";
 import PosterFallback from "../../assets/no-poster.png";
+import CircleRating from "../circleRating/CircleRating";
 
 import "./style.scss";
 
@@ -71,7 +72,7 @@ const Carousel = ({ data, loading, endpoint, title }) => {
                 >
                   <div className="posterBlock">
                     <Img src={posterUrl} />
-                    {/* Removed CircleRating and Genres */}
+                    <CircleRating rating={item.vote_average.toFixed(1)} />
                   </div>
                   <div className="textBlock">
                     <span className="title">{item.title || item.name}</span>
