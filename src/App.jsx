65c8f58fc5c3c8ddce108dcad4efd57130bb613b
Aryan Kamat <aryankamat03@gmail.com>
2025-03-10 19:56:58 +0530

    Explore page added

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { getApiConfiguration, getGenres } from "./store/homeSlice";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
 import Home from "./pages/home/Home";
 
 function App() {
@@ -55,8 +56,9 @@ function App() {
     <BrowserRouter>
       <Header />
       <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
       </Routes>
     </BrowserRouter>
   );
