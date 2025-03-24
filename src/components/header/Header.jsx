099d75b935aac539a53130d019692b3bebf2af4c
Aryan Kamat <aryankamat03@gmail.com>
2025-03-24 11:24:25 +0530

    Header Update ( auto-hide on scroll)

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { HiOutlineSearch } from "react-icons/hi";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 import "./style.scss";
 import ContentWrapper from "../contentWrapper/ContentWrapper";
 import logo from "../../assets/movix-logo.svg";
 
 const Header = () => {
+    const [show, setShow] = useState("top");
+    const [lastScrollY, setLastScrollY] = useState(0);
     const [query, setQuery] = useState("");
     const navigate = useNavigate();
+    const location = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [location]);
+
+    const controlNavbar = () => {
+        if (window.scrollY > 200) {
+            if (window.scrollY > lastScrollY) {
+                setShow("hide");
+            } else {
+                setShow("show");
+            }
+        } else {
+            setShow("top");
+        }
+        setLastScrollY(window.scrollY);
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", controlNavbar);
+        return () => {
+            window.removeEventListener("scroll", controlNavbar);
+        };
+    }, [lastScrollY]);
 
     const searchQueryHandler = (event) => {
         if (event.key === "Enter" && query.length > 0) {
@@ -17,7 +44,7 @@ const Header = () => {
     };
 
     return (
-        <header className="header">
+        <header className={`header ${show}`}>
             <ContentWrapper>
                 <div className="logo" onClick={() => navigate("/")}> 
                     <img src={logo} alt="Movix Logo" />
