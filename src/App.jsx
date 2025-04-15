a1c0c3cb6782f3c47de5e702388b4c1ea5002738
Aryan Kamat <aryankamat03@gmail.com>
2025-04-15 14:18:49 +0530

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import { useSelector, useDispatch } from "react-redux";
 import { getApiConfiguration, getGenres } from "./store/homeSlice";
 
 import Header from "./components/header/Header";
-import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
 import Details from "./pages/details/Details";
 import SearchResult from "./pages/searchResult/SearchResult";
@@ -62,7 +61,6 @@ function App() {
         <Route path="/search/:query" element={<SearchResult />} />
         <Route path="/explore/:mediaType" element={<Explore />} />
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
 }
